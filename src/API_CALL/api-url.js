655c1a00e3341d2629d798250945d950c48af847
Manoj Kumar N <manojcsengineer@gmail.com>
2024-03-09 13:49:14 +0530

    added-deployment-configurations

--- src/API_CALL/api-url.js
+++ src/API_CALL/api-url.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000";
+// const API_URL = "http://localhost:5000";
+const API_URL = "https://mern-chat-app-backend-4omg.onrender.com";
 
 const BASE_APP_URL = "http://localhost:3000/";
 
